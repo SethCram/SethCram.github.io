ed421b93b987cc63daa5e94ab0fb6014cb3da57f
sethhi2 <sethcram@gmail.com>
2022-10-09 13:16:42 -0600

    components setup and default css

--- portfolio_website/src/App.jsx
+++ portfolio_website/src/App.jsx
@@ -1,9 +1,28 @@
 //jsx bc easier to access images with this than js
 import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import Services from './components/services/Services.jsx'
+import Portfolio from './components/portfolio/Portfolio.jsx'
+import Testimonials from './components/testimonials/Testimonials.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 const app = () => {
   return (
-    <div>app</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
