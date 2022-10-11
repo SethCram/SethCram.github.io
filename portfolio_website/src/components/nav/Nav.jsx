0b634eba8b6a69848811f79ba0660c94cbc3f8a3
sethhi2 <sethcram@gmail.com>
2022-10-10 21:43:38 -0600

    portfolio finished besides Dungeon Jump link

--- portfolio_website/src/components/nav/Nav.jsx
+++ portfolio_website/src/components/nav/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolder /></a>
-      <a href="#portfolio" onClick={() => setActiveNav('#work_history')} className={activeNav === '#work_history' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#work_history" onClick={() => setActiveNav('#work_history')} className={activeNav === '#work_history' ? 'active' : ''}><RiServiceLine /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
--- portfolio_website/src/components/portfolio/Portfolio.jsx
+++ portfolio_website/src/components/portfolio/Portfolio.jsx
@@ -1,22 +1,57 @@
 import React from 'react'
 import './portfolio.css'
+import Dungeon_Jump from '../../assets/Dungeon_Jump.png'
+import BestBuySearch from '../../assets/BestBuySearch.PNG'
+import Website from '../../assets/website.PNG'
 
+/* {} indicated in JS mode */
 /* blank target opens inna new tab */
 const Portfolio = () => {
+  const portfolioItems = [
+    {
+      id: 1,
+      image: BestBuySearch,
+      title: 'Ecommerce Product Matching Site',
+      github: 'https://github.com/SethCram/Best-Buy-Search-Project',
+      product: 'https://sethcram.pythonanywhere.com/BestBuySearch/login/',
+    },
+    {
+      id: 2,
+      image: Website,
+      title: 'React Portolio Website',
+      github: 'https://github.com/SethCram/SethCram.github.io',
+      product: 'https://sethcram.github.io/',
+    },
+    {
+      id: 3,
+      image: Dungeon_Jump,
+      title: '2D Variable-Perspective Adventure Game',
+      github: 'https://github.com/3khoin/CS383Spring2022',
+      product: '',
+    }
+  ]
+  
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-
-          </div>
-          <h3>Portfolio item title</h3>
-          <a href="" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Product</a>
-        </article>
+        {
+          portfolioItems.map(({ id, image, title, github, product }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={product} className='btn btn-primary' target='_blank'>Product</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
