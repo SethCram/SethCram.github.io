245774356fcea4ba9c26b4bb5e3117134269c590
sethhi2 <sethcram@gmail.com>
2022-10-15 13:33:57 -0600

    message sent with alert if successful

--- portfolio_website/src/components/contact/Contact.jsx
+++ portfolio_website/src/components/contact/Contact.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
 import { BsLinkedin } from 'react-icons/bs'
+import emailjs from 'emailjs-com';
+import {BiMessageCheck} from 'react-icons/bi'
 
 function ContactOption(props) {
   return (
@@ -15,13 +17,33 @@ function ContactOption(props) {
 }
 
 const Contact = () => {
+  const form = useRef();
+
+  const [isAlertVisible, setIsAlertVisible] = React.useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wexjbdc', 'template_va1z0iq', form.current, '1DNRiNOEGKnoarC5z')
+      .then((result) => {
+        e.target.reset(); /* only reset form if sent successfully */
+        console.log(result.text);
+        /* Set msg alert to visible for a time */
+        setIsAlertVisible(true);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
+    
     <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
       <div className='container contact__container'>
         <div className='contact__options'>
+          
           <ContactOption
           icon=<MdOutlineEmail className='contact__option-icon' />
             contact_type='Email'
@@ -34,15 +56,26 @@ const Contact = () => {
             contact_point='Seth Cram'
             link='https://www.linkedin.com/in/seth-cram/'
           />
+          {/* Alert included in the contact options container */ }
+          {
+            <div
+              className={`contact__alert-container alert-success ${isAlertVisible ? 'alert-shown' : 'alert-hidden'}`}
+              onTransitionEnd={() => setIsAlertVisible(false)}
+            >
+              <BiMessageCheck className='contact__alert-icon' />
+              <h5>Message Sent</h5>
+            </div>
+          }
         </div>
         {/* END OF CONTACT OPTIONS */ }
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="email" placeholder='Your Email' required />
+          <input type="email" name='email' placeholder='Your Email' required />
           <textarea name='message' rows='7' placeholder='Your Message' required />
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
       </div>
+
     </section>
   )
 }
