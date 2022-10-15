178f75236fe0ca0e25cea4f082fe69eb9b17e35f
sethhi2 <sethcram@gmail.com>
2022-10-14 21:48:01 -0600

    contact form setup but not functional

--- portfolio_website/src/components/contact/Contact.jsx
+++ portfolio_website/src/components/contact/Contact.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { BsLinkedin } from 'react-icons/bs'
+
+function ContactOption(props) {
+  return (
+    <article className='contact__option'>
+      {props.icon}
+      <h4>{props.contact_type}</h4>
+      <h5>{props.contact_point}</h5>
+      <a href={props.link} target="_blank" rel="noreferrer">Send a message</a>
+    </article>
+  )
+}
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <ContactOption
+          icon=<MdOutlineEmail className='contact__option-icon' />
+            contact_type='Email'
+            contact_point='sethcram@gmail.com'
+            link='mailto:sethcram@gmail.com'
+          />
+          <ContactOption
+          icon=<BsLinkedin className='contact__option-icon' />
+            contact_type='LinkedIn'
+            contact_point='Seth Cram'
+            link='https://www.linkedin.com/in/seth-cram/'
+          />
+        </div>
+        {/* END OF CONTACT OPTIONS */ }
+        <form action=''>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required />
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
